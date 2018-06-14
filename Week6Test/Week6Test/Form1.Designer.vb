9ce618a0f58230045ab0971a1493383e2adba7b5
anwar.abdalbari@uoit.ca <Omar24112004>
2018-06-14 02:27:03 -0400

    initial commit

--- Week6Test/Week6Test/Form1.Designer.vb
+++ Week6Test/Week6Test/Form1.Designer.vb
@@ -25,6 +25,7 @@ Partial Class Form1
         Me.btnExit = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btnExit
@@ -53,11 +54,21 @@ Partial Class Form1
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Label1"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(83, 172)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(468, 348)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.btnExit)
@@ -71,4 +82,5 @@ Partial Class Form1
     Friend WithEvents btnExit As Button
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
