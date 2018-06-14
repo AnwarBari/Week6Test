b2be61dca715138509909460cd5204311b703862
anwar.abdalbari@uoit.ca <Omar24112004>
2018-06-14 02:36:47 -0400

    initial commit

--- Week6Test/Week6Test/Form1.vb
+++ Week6Test/Week6Test/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Form1
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
-
+        'sdsdsdsdsd'
+       'ddddd
     End Sub
 End Class
